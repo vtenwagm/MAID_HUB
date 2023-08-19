7db09ab70471d66f42fbdc1b8b9d8acb0d7622c8
vten <89058645+vtenwagm@users.noreply.github.com>
2023-08-19 11:40:54 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,9 @@
---script
+device = game:GetService("RbxAnalyticsService"):GetClientId()
+setclipboard(tostring(device))
+name = game:HttpGet("https://maidhubservice.vtn1001.repl.co/api/device/check?device="..device)
+
+if name == "none" then loadstring(game:HttpGet("https://raw.githubusercontent.com/vtenwagm/MAID_HUB/main/active.lua"))()
+
+else loadstring(game:HttpGet("https://raw.githubusercontent.com/vtenwagm/MAID_HUB/main/main.lua"))()
+
+end
