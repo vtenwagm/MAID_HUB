6b266c89edd7302263808d4284e8d8a04b7f947f
vten <89058645+vtenwagm@users.noreply.github.com>
2024-06-02 18:31:22 +0700

    Update bfh.lua

--- bfh.lua
+++ bfh.lua
@@ -13,7 +13,7 @@ local L_4_ = L_1_:CreateWindow({
 	TabWidth = 160,
 	Size = UDim2.fromOffset(530, 350),
 	Acrylic = false,
-	Theme = "Aqua",
+	Theme = "Black",
 	MinimizeKey = Enum.KeyCode.End
 })
 local L_5_ = {
